0deea066915ad3e2af60a07dd3771d2a50d59563
Fidel Yiu <fidel-yiu@foxmail.com>
2022-03-31 18:55:06 +0800

    chore: base rollup code

---
+++ rollup.config.ts
@@ -0,0 +1,15 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import type { RollupOptions } from "rollup";
+
+const config: RollupOptions = {
+    input: "src/index.ts",
+    output: {
+        file: "dist/index.js",
+        format: "cjs",
+    },
+    plugins: [typescript(), nodeResolve(), commonjs()],
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@
 import * as Lang from "./lang/clone";
 export { Lang };
 
-// number方面的工具
-import * as NumIs from "./num/num-is";
-import * as NumType from "./num/num-type";
-export { NumIs, NumType };
+// // number方面的工具
+// import * as NumIs from "./num/num-is";
+// import * as NumType from "./num/num-type";
+// export { NumIs, NumType };
 
-// tree方面的工具
-import * as TreeGet from "./tree/tree-get";
-import * as TreeHas from "./tree/tree-has";
-import * as TreeOp from "./tree/tree-op";
-import * as TreeType from "./tree/tree-type";
-export { TreeGet, TreeHas, TreeOp, TreeType };
+// // tree方面的工具
+// import * as TreeGet from "./tree/tree-get";
+// import * as TreeHas from "./tree/tree-has";
+// import * as TreeOp from "./tree/tree-op";
+// import * as TreeType from "./tree/tree-type";
+// export { TreeGet, TreeHas, TreeOp, TreeType };
