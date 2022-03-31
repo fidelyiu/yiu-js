7bc639b947a8d569afc6f6a7f56fd9189978d132
Fidel Yiu <fidel-yiu@foxmail.com>
2022-03-31 17:44:28 +0800

    chore: add some type

--- src/tree/tree-type.ts
+++ src/tree/tree-type.ts
@@ -2,14 +2,32 @@ type TreeOtherData<T> = {
     [P in keyof Omit<T, "children">]: Omit<T, "children">[P];
 };
 
+/**
+ * 树节点类型
+ */
 export type TreeNode<T = Record<string, unknown>> = {
+    /**
+     * 子树
+     */
     children: TreeData<T>;
 } & TreeOtherData<T>;
 
+/**
+ * 树类型
+ */
 export type TreeData<T = Record<string, unknown>> = Array<TreeNode<T>>;
 
+/**
+ * 树的搜索函数类型
+ */
 export type TreeSearchFunc<T = Record<string, unknown>> = (treeNode: TreeNode<T>) => boolean;
+/**
+ * 树的操作函数类型
+ */
 export type TreeOperationFunc<T = Record<string, unknown>> = (treeNode: TreeNode<T>) => void;
+/**
+ * 树的过滤配置类型
+ */
 export type TreeFilterOption = {
     /**
      * 默认false，父节点是否必须 需要匹配
