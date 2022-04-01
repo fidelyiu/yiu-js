f67390af3b2825e8e17367aa66cbf4f45b2ddc33
Fidel Yiu <fidel-yiu@foxmail.com>
2022-04-01 19:37:21 +0800

    chore: 打包字符串工具

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,8 @@ import * as TreeHas from "./tree/tree-has";
 import * as TreeOp from "./tree/tree-op";
 import * as TreeType from "./tree/tree-type";
 export { TreeGet, TreeHas, TreeOp, TreeType };
+
+// string方面的工具
+import * as StrIs from "./str/str-is";
+import * as StrGet from "./str/str-get";
+export { StrIs, StrGet };
--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,3 +1,1 @@
-import { deepClone } from "./clone";
-
-export { deepClone };
+export {};
