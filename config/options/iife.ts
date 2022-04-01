86f7f077669f051ea74157475bbb4099910c3082
Fidel Yiu <fidel-yiu@foxmail.com>
2022-04-01 17:10:20 +0800

    chore: 放开所有模块

--- config/options/iife.ts
+++ config/options/iife.ts
@@ -1,4 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import type { RollupOptions } from "rollup";
 
@@ -10,7 +12,7 @@ import type { RollupOptions } from "rollup";
 export function createOptionIife(fileName: string, outName: string, minify = false): RollupOptions {
     return {
         input: "src/index.ts",
-        plugins: [typescript()],
+        plugins: [typescript(), nodeResolve(), commonjs()],
         output: {
             name: outName,
             file: `dist/${fileName}.iife${minify ? ".min" : ""}.js`,
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@
  */
 
 // js语言方面的工具
-// import * as Lang from "./lang/clone";
-// export { Lang };
+import * as Lang from "./lang/clone";
+export { Lang };
 
 // number方面的工具
 import * as NumIs from "./num/num-is";
@@ -12,8 +12,8 @@ import * as NumType from "./num/num-type";
 export { NumIs, NumType };
 
 // tree方面的工具
-// import * as TreeGet from "./tree/tree-get";
-// import * as TreeHas from "./tree/tree-has";
-// import * as TreeOp from "./tree/tree-op";
-// import * as TreeType from "./tree/tree-type";
-// export { TreeGet, TreeHas, TreeOp, TreeType };
+import * as TreeGet from "./tree/tree-get";
+import * as TreeHas from "./tree/tree-has";
+import * as TreeOp from "./tree/tree-op";
+import * as TreeType from "./tree/tree-type";
+export { TreeGet, TreeHas, TreeOp, TreeType };
