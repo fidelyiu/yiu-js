808705dfa13969b547177eb3f9681826667259e3
Fidel Yiu <fidel-yiu@foxmail.com>
2022-04-08 10:10:10 +0800

    chore: 去除rollup警告

--- src/lang/lang-is.ts
+++ src/lang/lang-is.ts
@@ -1,18 +1,19 @@
-import { isNumber, isFinite } from "lodash";
+import { isNumber as _isNumber, isFinite as _isFinite } from "lodash";
 
-export {
-    /**
-     * 检查值是否是 Number 或者 Number对象
-     * 注意：要排除`Infinity`、`-Infinity`和`NaN`，使用`isFinite`方法
-     * @param value 要检查的值
-     * @return `true` 表示是Number 或者 Number对象
-     */
-    isNumber,
-    /**
-     * 检查值是否是一个有效数
-     * 注意：这个方法是基于`Number.isFinite`的
-     * @param value 要检查的值
-     * @return `true` 表示是有效值
-     */
-    isFinite,
-};
+/**
+ * 检查值是否是 Number 或者 Number对象
+ * 注意：要排除`Infinity`、`-Infinity`和`NaN`，使用`isFinite`方法
+ * @param value 要检查的值
+ * @return `true` 表示是Number 或者 Number对象
+ */
+const isNumber = _isNumber;
+
+/**
+ * 检查值是否是一个有效数
+ * 注意：这个方法是基于`Number.isFinite`的
+ * @param value 要检查的值
+ * @return `true` 表示是有效值
+ */
+const isFinite = _isFinite;
+
+export { isNumber, isFinite };
