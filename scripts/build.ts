7e0a40691cde960e21d905550d88cdb03cb64dcb
Fidel Yiu <fidel-yiu@foxmail.com>
2022-04-08 12:02:27 +0800

    chore: 修改ts配置文件

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,7 +3,7 @@ import { spawn } from "node:child_process";
 function bootstrap() {
     spawn(
         "node",
-        ["--experimental-vm-modules", "--no-warnings", "node_modules/rollup/dist/bin/rollup", "--config=./rollup/rollup.config.ts", " --configPlugin='@rollup/plugin-typescript'"],
+        ["--experimental-vm-modules", "--no-warnings", "node_modules/rollup/dist/bin/rollup", "--config=./rollup/rollup.config.ts", "--configPlugin=@rollup/plugin-typescript"],
         {
             stdio: "inherit",
         }
