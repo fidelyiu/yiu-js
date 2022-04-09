a1da8671dddc81b79380dcc08d2ee0bf36ec0995
Fidel Yiu <fidel-yiu@foxmail.com>
2022-04-09 23:43:02 +0800

    chore: 在glup获取jest的版本

--- gulp/index.ts
+++ gulp/index.ts
@@ -1,6 +1,8 @@
 import type { TaskFunction } from "gulp";
+import { getVersion } from "jest";
 
 const defaultTask: TaskFunction = function (cb) {
+    console.log("jestVersion", getVersion());
     cb();
 };
 
